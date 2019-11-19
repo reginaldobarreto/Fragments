cf81cbc48f69fe18ae50ba2853580379a7a88d92
Reginaldo Barreto <reginaldo.barreto@gmail.com>
2019-11-19 01:13:33 -0300

    add suporte a edição dados AFragment

--- app/src/main/java/br/com/barrsoft/fragments/fragments/AFragment.java
+++ app/src/main/java/br/com/barrsoft/fragments/fragments/AFragment.java
@@ -1,16 +1,12 @@
 package br.com.barrsoft.fragments.fragments;
 
 
-import android.app.Activity;
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
 import br.com.barrsoft.fragments.R;
 import butterknife.BindView;
 import butterknife.ButterKnife;
